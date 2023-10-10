0abb73118b51ad667dbbc1fe9011ad2add22e1a0
Daniel Vinhas <vinhas.daniel@gmail.com>
2023-10-10 08:56:00 -0300

    tipos de variáveis no Java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // precisa ser feito Cast para que a compilação dê certo
+        // O Cast é uma maneira de dizer que estamos cientes de que o novo tipo é menos preciso do que
+        // o tipo original
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; // não pode ser alterado
     }
 }
