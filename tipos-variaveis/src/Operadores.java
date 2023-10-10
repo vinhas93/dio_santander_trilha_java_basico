f825e7a59c1592393665af2056f8e18e4c33e04c
Daniel Vinhas <vinhas.daniel@gmail.com>
2023-10-10 10:05:47 -0300

    tipos de operadores no Java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+    public static void main(String[] args) {
+//        String nomeCompleto = "LINGUAGEM" + "jAVA";
+//        System.out.println(nomeCompleto); // LINGUAGEMjAVA
+
+//        String concatenacao = "?";
+//
+//        concatenacao = 1 + 1 + 1 + "1";
+//
+//        System.out.println(concatenacao);
+//
+//        concatenacao = 1 + "1" + 1 + 1;
+//
+//        System.out.println(concatenacao);
+//
+//        concatenacao = 1 + "1" + 1 + "1";
+//
+//        System.out.println(concatenacao);
+//
+//        concatenacao = "1" + 1 + 1 + 1;
+//
+//        System.out.println(concatenacao);
+//
+//        concatenacao = "1" + (1 + 1 + 1);
+//
+//        System.out.println(concatenacao);
+
+
+
+    }
+}
